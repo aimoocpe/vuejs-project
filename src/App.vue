d65c224ed9923a21713128f1aefa098e3b516d61
THEERAWAT KAEWCHOTE <theerawatkc@gmail.com>
2016-11-28 01:26:41 +0700

    course vue.js day2

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <input v-model="name">
+    <br>
+    <li><input type="checkbox" value="6" v-model="list"> 7 </li>
+    <li><input type="checkbox" value="7" v-model="list"> 8 </li>
+    <li><input type="checkbox" value="8" v-model="list"> 9 </li>
+    <br>
+    <hello :name="name" :list="list"></hello>
+    <br>
+    <button v-on:click="sayHello()">Say</button>
+    
+  </div>
+</template>
+
+<script>
+import Hello from './components/Hello.vue'
+
+export default {
+  data () {
+    return {
+      name: '',
+      list: [
+        1, 2, 3, 4, 5
+      ]
+    }
+  },
+  components: {
+    Hello
+  },
+  methods: {
+    sayHello () {
+      window.alert('Hi, ' + this.name)
+    }
+  }
+}
+</script>
+
---
+++ src/components/Hello.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <p v-if="name && name !== 'aimoocpe'">Hi , {{name}}</p>
+    <p v-else-if="name == 'aimoocpe'">XD</p>
+    <p v-else>No Data</p>
+    <li v-for="x in list">{{ x }}</li>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    name: {
+      required: true
+    },
+    list: {
+      type: Array
+    }
+  }
+}
+</script>
+
